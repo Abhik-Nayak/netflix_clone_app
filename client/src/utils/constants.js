5034be284a22e393682cd498c504513011a1fc5c
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-02-09 05:28:15 +0530

    Abhik//09-02-2023

---
+++ client/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const API_KEY = "36dc4954ce6e1ad10206d96e0f606b57";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+// https://api.themoviedb.org/3/movie/550?api_key=36dc4954ce6e1ad10206d96e0f606b57
